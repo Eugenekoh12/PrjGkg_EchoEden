456c86e05788e2e44cdbf3da5aa842e0b0cc5e4c
shagana <nahashaa@gmail.com>
2024-07-11 21:15:08 +0800

    Update

--- main.py
+++ main.py
@@ -2,11 +2,13 @@ from flask import Flask, render_template, request, redirect, url_for, session, f
 from flask_mysqldb import MySQL
 from flask_bcrypt import Bcrypt
 from forms import RegistrationForm
+from flask_mail import Mail, Message
 import MySQLdb.cursors
 import re
 
 app = Flask(__name__)
 bcrypt = Bcrypt(app)
+mail = Mail(app)
 
 app.secret_key = 'the722semanticTOBOGGANS5smoothly.leutinizesTHEpointy3barrelOFgunpowder'
 app.config['MYSQL_HOST'] = 'localhost'
@@ -15,6 +17,15 @@ app.config['MYSQL_PASSWORD'] = 'Pan4will.FLEXmy7adviser'
 app.config['MYSQL_DB'] = 'echoeden'
 app.config['MYSQL_PORT'] = 3306
 mysql = MySQL(app)
+
+# Flask-Mail configuration
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USE_SSL'] = True
+app.config['MAIL_USERNAME'] = 'echoedenn@gmail.com'
+app.config['MAIL_PASSWORD'] = 'echoeden123'
+mail = Mail(app)
+
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
@@ -48,7 +59,7 @@ def register():
         cursor.close()
 
         flash('Account created for {}! You can now log in.'.format(username), 'success')
-        return redirect(url_for('login'))
+        return redirect(url_for('login'))did
 
     return render_template('register.html', title='Register', form=form)
 
@@ -71,6 +82,8 @@ def login():
 
         if bcrypt.check_password_hash(stored_hashed_password, password):
             flash('Login successful!', 'success')
+            # Send email notification
+            send_login_notification(account['email'], username)
             return redirect(url_for('home'))
         else:
             flash('Invalid username or password', 'danger')
@@ -81,6 +94,14 @@ def login():
 @app.route('/home')
 def home():
     return "Welcome to the Home Page"
+
+def send_login_notification(email, username):
+    msg = Message('Login Attempt Notification',
+                  sender='your-email@gmail.com',
+                  recipients=[email])
+    msg.body = f"Hello {username},\n\nSomeone has attempted to log into your account on ExampleApp. If this was you, you can ignore this message. If not, please take appropriate action."
+    mail.send(msg)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
