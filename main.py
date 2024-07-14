bbeca6f857d8695ce285cb8440286e17b975081a
Eugenekoh12 <12463556+Eugenekoh12@users.noreply.github.com>
2024-07-15 00:32:32 +0800

    Added preventive measures for associates to work before anything.

--- main.py
+++ main.py
@@ -52,19 +52,21 @@ def register():
         mysql.connection.commit()
         settings_id = cursor.lastrowid
 
-        # Insert new user into the accounts table
+        # Insert new user into the accounts table and get the user_id
         cursor.execute(
             "INSERT INTO accounts (username, email, password_hash, settings_id) VALUES (%s, %s, %s, %s)",
             (username, email, hashed_password, settings_id)
         )
         mysql.connection.commit()
-
-        # Get the user's ID
         user_id = cursor.lastrowid
 
         # Insert a new record into the account_settings table for the new user
         cursor.execute(
             "INSERT INTO account_settings (user_id) VALUES (%s)",
+            (user_id,)
+        )
+        cursor.execute(
+            "INSERT INTO associates (user_id) VALUES (%s)",
             (user_id,)
         )
         mysql.connection.commit()
